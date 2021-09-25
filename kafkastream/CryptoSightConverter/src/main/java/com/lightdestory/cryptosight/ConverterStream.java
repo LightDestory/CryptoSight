e1c8a22f0596a5baee8e5e6959b82ae178b723d5
LightDestory <apb231@gmail.com>
2021-09-25 21:55:44 +0200

    feat: Nice to meet you opensearch-dashboards

---
+++ kafkastream/CryptoSightConverter/src/main/java/com/lightdestory/cryptosight/ConverterStream.java
@@ -0,0 +1,78 @@
+package com.lightdestory.cryptosight;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.KeyValue;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+public class ConverterStream {
+
+    final static String KAFKA_ADDRESS = "kafkaserver:9092";
+    final static String RAW_TOPIC = "cryptocurrencies-raw";
+    final static String PROCESSED_TOPIC = "cryptocurrencies";
+    final static String APP_NAME = "CryptoSight";
+
+    public static void main(String[] args) {
+        // Creating the StreamBuilder instance
+        final StreamsBuilder builder = new StreamsBuilder();
+        // Assigning workload, parsing every KeyValue topic's message into a new one
+        // Saving the new generated data into a second topic
+        builder.<String, String>stream(RAW_TOPIC)
+                .map((k, v) -> new KeyValue<>(k, generateCleanDataJSON(v)))
+                .to(PROCESSED_TOPIC);
+        // Initializing Stream
+        final Topology topology = builder.build();
+        final KafkaStreams streams = new KafkaStreams(topology, createProps());
+        // Multithreading Counter
+        final CountDownLatch latch = new CountDownLatch(1);
+        // Attaching shutdown handler to catch control-c
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            // Closes the Stream
+            streams.close();
+            // Put the counter to 0, releasing the main thread from the freeze
+            latch.countDown();
+        }, String.format("streams-%s-shutdown", APP_NAME)));
+        // Running the stream
+        try {
+            // Start the stream
+            streams.start();
+            // Put the main thread to a freeze state
+            latch.await();
+        } catch (Throwable e) {
+            System.exit(1);
+        }
+        System.exit(0);
+    }
+
+    private static Properties createProps() {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, String.format("streams-%s", APP_NAME));
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_ADDRESS);
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        return props;
+    }
+
+    private static String generateCleanDataJSON(String value) {
+        JSONObject jsonObject = new JSONObject(value);
+        JSONArray raw_data = new JSONArray(jsonObject.getString("message"));
+        jsonObject.remove("message");
+        raw_data.forEach(element -> {
+            JSONObject obj = (JSONObject) element;
+            String name = obj.getString("id");
+            double price_usd = obj.getDouble("current_price");
+            double rounded_price_eur = Math.round((price_usd*0.85)*100)/100.00;
+            jsonObject.put(String.format("%s_current_price_usd", name), price_usd);
+            jsonObject.put(String.format("%s_current_price_eur", name), rounded_price_eur);
+            jsonObject.put(String.format("%s_market_cap", name), obj.getDouble("market_cap"));
+            jsonObject.put(String.format("%s_circulating_supply", name), obj.getDouble("circulating_supply"));
+        });
+        return jsonObject.toString();
+    }
+}
